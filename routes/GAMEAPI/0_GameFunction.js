2617cde6547e2b2f1f1b1c347a49c50767d48fc6
bolt1688 <mrcmrc3399@gmail.com>
2025-11-26 22:25:55 +0800

    Update 0_GameFunction.js

--- routes/GAMEAPI/0_GameFunction.js
+++ routes/GAMEAPI/0_GameFunction.js
@@ -65,7 +65,7 @@ const GAME_BALANCE_CHECKERS = [
 ];
 
 router.post(
-  "/admin/api/allgame/balance/:userId",
+  "/api/allgame/balance/:userId",
   authenticateAdminToken,
   async (req, res) => {
     try {
