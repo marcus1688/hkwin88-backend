fc2a8d2abb5845e127ed6b8e2496fbeb04cfcd2c
bolt1688 <mrcmrc3399@gmail.com>
2025-11-26 21:59:14 +0800

    Update slotjoker.js

--- routes/GAMEAPI/slotjoker.js
+++ routes/GAMEAPI/slotjoker.js
@@ -374,27 +374,49 @@ router.post(
   }
 );
 
-router.post("/api/joker/getbalance", authenticateToken, async (req, res) => {
-  try {
-    const userId = req.user.userId;
-    const user = await User.findById(userId);
+router.post(
+  "/api/joker/getbalance/:userId",
+  authenticateAdminToken,
+  async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const user = await User.findById(userId);
 
-    if (!user) {
+      if (!user) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "User not found. Please try again or contact customer service for assistance.",
+            zh: "用户未找到，请重试或联系客服以获取帮助。",
+            ms: "Pengguna tidak ditemui, sila cuba lagi atau hubungi khidmat pelanggan untuk bantuan.",
+            zh_hk: "搵唔到用戶，麻煩再試多次或者聯絡客服幫手。",
+            id: "Pengguna tidak ditemukan. Silakan coba lagi atau hubungi layanan pelanggan untuk bantuan.",
+          },
+        });
+      }
+
+      const balanceResponse = await JokerCheckBalance(user);
+
+      if (!balanceResponse.success) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "JOKER: Unable to retrieve player balance. Please try again or contact customer support for assistance.",
+            zh: "JOKER: 无法获取玩家余额。请重试或联系客服寻求帮助。",
+            ms: "JOKER: Tidak dapat mendapatkan baki pemain. Sila cuba lagi atau hubungi sokongan pelanggan untuk bantuan.",
+            zh_hk: "JOKER: 無法獲取玩家餘額。請重試或聯絡客服尋求協助。",
+            id: "JOKER: Tidak dapat mengambil saldo pemain. Silakan coba lagi atau hubungi dukungan pelanggan untuk bantuan.",
+          },
+        });
+      }
+
       return res.status(200).json({
-        success: false,
-        message: {
-          en: "User not found. Please try again or contact customer service for assistance.",
-          zh: "用户未找到，请重试或联系客服以获取帮助。",
-          ms: "Pengguna tidak ditemui, sila cuba lagi atau hubungi khidmat pelanggan untuk bantuan.",
-          zh_hk: "搵唔到用戶，麻煩再試多次或者聯絡客服幫手。",
-          id: "Pengguna tidak ditemukan. Silakan coba lagi atau hubungi layanan pelanggan untuk bantuan.",
-        },
+        success: true,
+        balance: balanceResponse.data.Credit,
+        outstanding: balanceResponse.data.OutstandingCredit,
       });
-    }
-
-    const balanceResponse = await JokerCheckBalance(user);
-
-    if (!balanceResponse.success) {
+    } catch (error) {
+      console.log("JOKER error fetching balance", error.message);
       return res.status(200).json({
         success: false,
         message: {
@@ -406,91 +428,111 @@ router.post("/api/joker/getbalance", authenticateToken, async (req, res) => {
         },
       });
     }
-
-    return res.status(200).json({
-      success: true,
-      balance: balanceResponse.data.Credit,
-      outstanding: balanceResponse.data.OutstandingCredit,
-    });
-  } catch (error) {
-    console.log("JOKER error fetching balance", error.message);
-    return res.status(200).json({
-      success: false,
-      message: {
-        en: "JOKER: Unable to retrieve player balance. Please try again or contact customer support for assistance.",
-        zh: "JOKER: 无法获取玩家余额。请重试或联系客服寻求帮助。",
-        ms: "JOKER: Tidak dapat mendapatkan baki pemain. Sila cuba lagi atau hubungi sokongan pelanggan untuk bantuan.",
-        zh_hk: "JOKER: 無法獲取玩家餘額。請重試或聯絡客服尋求協助。",
-        id: "JOKER: Tidak dapat mengambil saldo pemain. Silakan coba lagi atau hubungi dukungan pelanggan untuk bantuan.",
-      },
-    });
   }
-});
+);
 
-router.post("/api/joker/deposit", authenticateToken, async (req, res) => {
-  let formattedDepositAmount = 0;
-  try {
-    const { transferAmount } = req.body;
-    formattedDepositAmount = roundToTwoDecimals(transferAmount);
+router.post(
+  "/api/joker/deposit/:userId",
+  authenticateAdminToken,
+  async (req, res) => {
+    let formattedDepositAmount = 0;
+    try {
+      const { transferAmount } = req.body;
+      formattedDepositAmount = roundToTwoDecimals(transferAmount);
 
-    if (isNaN(formattedDepositAmount) || formattedDepositAmount <= 0) {
+      if (isNaN(formattedDepositAmount) || formattedDepositAmount <= 0) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "Deposit amount must be a positive number greater than 0.",
+            zh: "存款金额必须为正数且大于0。",
+            ms: "Jumlah deposit mestilah nombor positif dan lebih besar daripada 0.",
+            zh_hk: "存款金額必須為正數且大於0。",
+            id: "Jumlah deposit harus berupa angka positif dan lebih besar dari 0.",
+          },
+        });
+      }
+
+      const { userId } = req.params;
+      const user = await User.findById(userId);
+
+      if (!user) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "User not found. Please try again or contact customer service for assistance.",
+            zh: "用户未找到，请重试或联系客服以获取帮助。",
+            ms: "Pengguna tidak ditemui, sila cuba lagi atau hubungi khidmat pelanggan untuk bantuan.",
+            zh_hk: "搵唔到用戶，麻煩再試多次或者聯絡客服幫手。",
+            id: "Pengguna tidak ditemukan. Silakan coba lagi atau hubungi layanan pelanggan untuk bantuan.",
+          },
+        });
+      }
+
+      if (!user.jokerGameName) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "JOKER: Game account not registered. Please register an account first to proceed.",
+            zh: "JOKER: 游戏账户未注册。请先注册账户以继续。",
+            ms: "JOKER: Akaun permainan tidak berdaftar. Sila daftar akaun terlebih dahulu untuk meneruskan.",
+            zh_hk: "JOKER: 遊戲帳戶未註冊。請先註冊帳戶以繼續。",
+            id: "JOKER: Akun permainan belum terdaftar. Silakan daftar akun terlebih dahulu untuk melanjutkan.",
+          },
+        });
+      }
+
+      if (user.gameStatus.joker.transferInStatus) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "Transfer is temporarily locked. Please contact customer support for assistance.",
+            zh: "转账暂时锁定。请联系客服寻求帮助。",
+            ms: "Pemindahan dikunci buat sementara. Sila hubungi sokongan pelanggan untuk bantuan.",
+            zh_hk: "轉帳暫時鎖定。請聯絡客服尋求協助。",
+            id: "Transfer terkunci sementara. Silakan hubungi dukungan pelanggan untuk bantuan.",
+          },
+        });
+      }
+
+      const depositResponse = await JokerDeposit(user, formattedDepositAmount);
+
+      if (!depositResponse.success) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "JOKER: Deposit failed. Please try again or contact customer support for further assistance.",
+            zh: "JOKER: 存款失败。请重试或联系客服寻求进一步帮助。",
+            ms: "JOKER: Deposit gagal. Sila cuba lagi atau hubungi sokongan pelanggan untuk bantuan lanjut.",
+            zh_hk: "JOKER: 存款失敗。請重試或聯絡客服尋求進一步協助。",
+            id: "JOKER: Deposit gagal. Silakan coba lagi atau hubungi dukungan pelanggan untuk bantuan lebih lanjut.",
+          },
+        });
+      }
+
+      await GameWalletLogAttempt(
+        user.username,
+        "Transfer In",
+        "Transfer",
+        roundToTwoDecimals(formattedDepositAmount),
+        "JOKER",
+        roundToTwoDecimals(depositResponse.data.Credit || 0),
+        0,
+        0
+      );
+
       return res.status(200).json({
-        success: false,
+        success: true,
         message: {
-          en: "Deposit amount must be a positive number greater than 0.",
-          zh: "存款金额必须为正数且大于0。",
-          ms: "Jumlah deposit mestilah nombor positif dan lebih besar daripada 0.",
-          zh_hk: "存款金額必須為正數且大於0。",
-          id: "Jumlah deposit harus berupa angka positif dan lebih besar dari 0.",
+          en: "JOKER: Deposit completed successfully.",
+          zh: "JOKER: 存款成功完成。",
+          ms: "JOKER: Deposit berjaya diselesaikan.",
+          zh_hk: "JOKER: 存款成功完成。",
+          id: "JOKER: Deposit berhasil diselesaikan.",
         },
       });
-    }
-
-    const userId = req.user.userId;
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(200).json({
-        success: false,
-        message: {
-          en: "User not found. Please try again or contact customer service for assistance.",
-          zh: "用户未找到，请重试或联系客服以获取帮助。",
-          ms: "Pengguna tidak ditemui, sila cuba lagi atau hubungi khidmat pelanggan untuk bantuan.",
-          zh_hk: "搵唔到用戶，麻煩再試多次或者聯絡客服幫手。",
-          id: "Pengguna tidak ditemukan. Silakan coba lagi atau hubungi layanan pelanggan untuk bantuan.",
-        },
-      });
-    }
-
-    if (!user.jokerGameName) {
-      return res.status(200).json({
-        success: false,
-        message: {
-          en: "JOKER: Game account not registered. Please register an account first to proceed.",
-          zh: "JOKER: 游戏账户未注册。请先注册账户以继续。",
-          ms: "JOKER: Akaun permainan tidak berdaftar. Sila daftar akaun terlebih dahulu untuk meneruskan.",
-          zh_hk: "JOKER: 遊戲帳戶未註冊。請先註冊帳戶以繼續。",
-          id: "JOKER: Akun permainan belum terdaftar. Silakan daftar akun terlebih dahulu untuk melanjutkan.",
-        },
-      });
-    }
-
-    if (user.gameStatus.joker.transferInStatus) {
-      return res.status(200).json({
-        success: false,
-        message: {
-          en: "Transfer is temporarily locked. Please contact customer support for assistance.",
-          zh: "转账暂时锁定。请联系客服寻求帮助。",
-          ms: "Pemindahan dikunci buat sementara. Sila hubungi sokongan pelanggan untuk bantuan.",
-          zh_hk: "轉帳暫時鎖定。請聯絡客服尋求協助。",
-          id: "Transfer terkunci sementara. Silakan hubungi dukungan pelanggan untuk bantuan.",
-        },
-      });
-    }
-
-    const depositResponse = await JokerDeposit(user, formattedDepositAmount);
-
-    if (!depositResponse.success) {
+    } catch (error) {
+      console.log("JOKER error deposit", error.message);
       return res.status(200).json({
         success: false,
         message: {
@@ -502,107 +544,114 @@ router.post("/api/joker/deposit", authenticateToken, async (req, res) => {
         },
       });
     }
-
-    await GameWalletLogAttempt(
-      user.username,
-      "Transfer In",
-      "Transfer",
-      roundToTwoDecimals(formattedDepositAmount),
-      "JOKER",
-      roundToTwoDecimals(depositResponse.data.Credit || 0),
-      0,
-      0
-    );
-
-    return res.status(200).json({
-      success: true,
-      message: {
-        en: "JOKER: Deposit completed successfully.",
-        zh: "JOKER: 存款成功完成。",
-        ms: "JOKER: Deposit berjaya diselesaikan.",
-        zh_hk: "JOKER: 存款成功完成。",
-        id: "JOKER: Deposit berhasil diselesaikan.",
-      },
-    });
-  } catch (error) {
-    console.log("JOKER error deposit", error.message);
-    return res.status(200).json({
-      success: false,
-      message: {
-        en: "JOKER: Deposit failed. Please try again or contact customer support for further assistance.",
-        zh: "JOKER: 存款失败。请重试或联系客服寻求进一步帮助。",
-        ms: "JOKER: Deposit gagal. Sila cuba lagi atau hubungi sokongan pelanggan untuk bantuan lanjut.",
-        zh_hk: "JOKER: 存款失敗。請重試或聯絡客服尋求進一步協助。",
-        id: "JOKER: Deposit gagal. Silakan coba lagi atau hubungi dukungan pelanggan untuk bantuan lebih lanjut.",
-      },
-    });
   }
-});
+);
 
-router.post("/api/joker/withdraw", authenticateToken, async (req, res) => {
-  let formattedWithdrawAmount = 0;
-  try {
-    const { transferAmount } = req.body;
-    formattedWithdrawAmount = roundToTwoDecimals(transferAmount);
+router.post(
+  "/api/joker/withdraw/:userId",
+  authenticateAdminToken,
+  async (req, res) => {
+    let formattedWithdrawAmount = 0;
+    try {
+      const { transferAmount } = req.body;
+      formattedWithdrawAmount = roundToTwoDecimals(transferAmount);
 
-    if (isNaN(formattedWithdrawAmount) || formattedWithdrawAmount <= 0) {
+      if (isNaN(formattedWithdrawAmount) || formattedWithdrawAmount <= 0) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "Withdrawal amount must be a positive number greater than 0.",
+            zh: "提款金额必须为正数且大于0。",
+            ms: "Jumlah pengeluaran mestilah nombor positif dan lebih besar daripada 0.",
+            zh_hk: "提款金額必須為正數且大於0。",
+            id: "Jumlah penarikan harus berupa angka positif dan lebih besar dari 0.",
+          },
+        });
+      }
+
+      const { userId } = req.params;
+      const user = await User.findById(userId);
+
+      if (!user) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "User not found. Please try again or contact customer service for assistance.",
+            zh: "用户未找到，请重试或联系客服以获取帮助。",
+            ms: "Pengguna tidak ditemui, sila cuba lagi atau hubungi khidmat pelanggan untuk bantuan.",
+            zh_hk: "搵唔到用戶，麻煩再試多次或者聯絡客服幫手。",
+            id: "Pengguna tidak ditemukan. Silakan coba lagi atau hubungi layanan pelanggan untuk bantuan.",
+          },
+        });
+      }
+
+      if (!user.jokerGameName) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "JOKER: Game account not registered. Please register an account first to proceed.",
+            zh: "JOKER: 游戏账户未注册。请先注册账户以继续。",
+            ms: "JOKER: Akaun permainan tidak berdaftar. Sila daftar akaun terlebih dahulu untuk meneruskan.",
+            zh_hk: "JOKER: 遊戲帳戶未註冊。請先註冊帳戶以繼續。",
+            id: "JOKER: Akun permainan belum terdaftar. Silakan daftar akun terlebih dahulu untuk melanjutkan.",
+          },
+        });
+      }
+
+      if (user.gameStatus.joker.transferOutStatus) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "Transfer is temporarily locked. Please contact customer support for assistance.",
+            zh: "转账暂时锁定。请联系客服寻求帮助。",
+            ms: "Pemindahan dikunci buat sementara. Sila hubungi sokongan pelanggan untuk bantuan.",
+            zh_hk: "轉帳暫時鎖定。請聯絡客服尋求協助。",
+            id: "Transfer terkunci sementara. Silakan hubungi dukungan pelanggan untuk bantuan.",
+          },
+        });
+      }
+
+      const withdrawResponse = await JokerWithdraw(
+        user,
+        formattedWithdrawAmount
+      );
+
+      if (!withdrawResponse.success) {
+        return res.status(200).json({
+          success: false,
+          message: {
+            en: "JOKER: Withdrawal failed. Please try again or contact customer support for further assistance.",
+            zh: "JOKER: 提款失败。请重试或联系客服寻求进一步帮助。",
+            ms: "JOKER: Pengeluaran gagal. Sila cuba lagi atau hubungi sokongan pelanggan untuk bantuan lanjut.",
+            zh_hk: "JOKER: 提款失敗。請重試或聯絡客服尋求進一步協助。",
+            id: "JOKER: Penarikan gagal. Silakan coba lagi atau hubungi dukungan pelanggan untuk bantuan lebih lanjut.",
+          },
+        });
+      }
+
+      await GameWalletLogAttempt(
+        user.username,
+        "Transfer Out",
+        "Transfer",
+        roundToTwoDecimals(formattedWithdrawAmount),
+        "JOKER",
+        roundToTwoDecimals(withdrawResponse.data.Credit || 0),
+        0,
+        0
+      );
+
       return res.status(200).json({
-        success: false,
+        success: true,
         message: {
-          en: "Withdrawal amount must be a positive number greater than 0.",
-          zh: "提款金额必须为正数且大于0。",
-          ms: "Jumlah pengeluaran mestilah nombor positif dan lebih besar daripada 0.",
-          zh_hk: "提款金額必須為正數且大於0。",
-          id: "Jumlah penarikan harus berupa angka positif dan lebih besar dari 0.",
+          en: "JOKER: Withdrawal completed successfully.",
+          zh: "JOKER: 提款成功完成。",
+          ms: "JOKER: Pengeluaran berjaya diselesaikan.",
+          zh_hk: "JOKER: 提款成功完成。",
+          id: "JOKER: Penarikan berhasil diselesaikan.",
         },
       });
-    }
-
-    const userId = req.user.userId;
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(200).json({
-        success: false,
-        message: {
-          en: "User not found. Please try again or contact customer service for assistance.",
-          zh: "用户未找到，请重试或联系客服以获取帮助。",
-          ms: "Pengguna tidak ditemui, sila cuba lagi atau hubungi khidmat pelanggan untuk bantuan.",
-          zh_hk: "搵唔到用戶，麻煩再試多次或者聯絡客服幫手。",
-          id: "Pengguna tidak ditemukan. Silakan coba lagi atau hubungi layanan pelanggan untuk bantuan.",
-        },
-      });
-    }
-
-    if (!user.jokerGameName) {
-      return res.status(200).json({
-        success: false,
-        message: {
-          en: "JOKER: Game account not registered. Please register an account first to proceed.",
-          zh: "JOKER: 游戏账户未注册。请先注册账户以继续。",
-          ms: "JOKER: Akaun permainan tidak berdaftar. Sila daftar akaun terlebih dahulu untuk meneruskan.",
-          zh_hk: "JOKER: 遊戲帳戶未註冊。請先註冊帳戶以繼續。",
-          id: "JOKER: Akun permainan belum terdaftar. Silakan daftar akun terlebih dahulu untuk melanjutkan.",
-        },
-      });
-    }
-
-    if (user.gameStatus.joker.transferOutStatus) {
-      return res.status(200).json({
-        success: false,
-        message: {
-          en: "Transfer is temporarily locked. Please contact customer support for assistance.",
-          zh: "转账暂时锁定。请联系客服寻求帮助。",
-          ms: "Pemindahan dikunci buat sementara. Sila hubungi sokongan pelanggan untuk bantuan.",
-          zh_hk: "轉帳暫時鎖定。請聯絡客服尋求協助。",
-          id: "Transfer terkunci sementara. Silakan hubungi dukungan pelanggan untuk bantuan.",
-        },
-      });
-    }
-
-    const withdrawResponse = await JokerWithdraw(user, formattedWithdrawAmount);
-
-    if (!withdrawResponse.success) {
+    } catch (error) {
+      console.log("JOKER error deposit", error.message);
       return res.status(200).json({
         success: false,
         message: {
@@ -614,42 +663,8 @@ router.post("/api/joker/withdraw", authenticateToken, async (req, res) => {
         },
       });
     }
-
-    await GameWalletLogAttempt(
-      user.username,
-      "Transfer Out",
-      "Transfer",
-      roundToTwoDecimals(formattedWithdrawAmount),
-      "JOKER",
-      roundToTwoDecimals(withdrawResponse.data.Credit || 0),
-      0,
-      0
-    );
-
-    return res.status(200).json({
-      success: true,
-      message: {
-        en: "JOKER: Withdrawal completed successfully.",
-        zh: "JOKER: 提款成功完成。",
-        ms: "JOKER: Pengeluaran berjaya diselesaikan.",
-        zh_hk: "JOKER: 提款成功完成。",
-        id: "JOKER: Penarikan berhasil diselesaikan.",
-      },
-    });
-  } catch (error) {
-    console.log("JOKER error deposit", error.message);
-    return res.status(200).json({
-      success: false,
-      message: {
-        en: "JOKER: Withdrawal failed. Please try again or contact customer support for further assistance.",
-        zh: "JOKER: 提款失败。请重试或联系客服寻求进一步帮助。",
-        ms: "JOKER: Pengeluaran gagal. Sila cuba lagi atau hubungi sokongan pelanggan untuk bantuan lanjut.",
-        zh_hk: "JOKER: 提款失敗。請重試或聯絡客服尋求進一步協助。",
-        id: "JOKER: Penarikan gagal. Silakan coba lagi atau hubungi dukungan pelanggan untuk bantuan lebih lanjut.",
-      },
-    });
   }
-});
+);
 
 module.exports = router;
 module.exports.registerJokerUser = registerJokerUser;
